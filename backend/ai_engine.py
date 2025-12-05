e13941be6044cb6c0e8a39721a307b072d73285d
Dmitribear <juventus.kon1@gmail.com>
2025-12-06 00:36:27 +0500

    replace with local version

--- backend/ai_engine.py
+++ backend/ai_engine.py
@@ -2,7 +2,6 @@ import os
 
 import openai
 
-openai.api_key = os.getenv("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")
 
 
 def analyze_object_openai(obj):
